d649791cb7ba8f34b21e348df99a1b8e8dafe4d5
Sam Marks <sam@sammarks.me>
2020-04-13 16:45:36 -0400

    feat: add gain icon

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { SVGFactory } from 'react';
 
 import _Metronome from './svgs/metronome.svg';
 import _Pitch from './svgs/pitch.svg';
+import _Gain from './svgs/gain.svg';
 
 const Metronome: SVGFactory = _Metronome;
 const Pitch: SVGFactory = _Pitch;
+const Gain: SVGFactory = _Gain;
 
-export { Metronome, Pitch };
+export { Metronome, Pitch, Gain };
